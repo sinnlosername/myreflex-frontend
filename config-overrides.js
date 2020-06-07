083b97babf7b985b6f860603dadc6b17085cfda3
Florian <me@florian.ws>
2020-06-07 21:12:00 +0200

    Add less loader

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const {
+  override,
+  addLessLoader
+} = require("customize-cra");
+
+module.exports = override(
+  addLessLoader({
+    lessOptions: {
+      javascriptEnabled: true,
+      modifyVars: {
+        '@primary-color': '#1890ff'
+      }
+    }
+  })
+)
