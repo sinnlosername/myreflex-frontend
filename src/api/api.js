4e0836037896774a0aa608d1e94bbabfaa87d766
Florian <me@florian.ws>
2020-06-11 23:27:50 +0200

    remove proxy; add error code to result

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,4 @@
-const API_URL = "https://my.reflex.rip/api/v1/auth?cookie=true";
-const DEV_API_PROXY = "http://localhost:8010/proxy";
+const API_URL = "https://my.reflex.rip/api/v1";
 
 export async function callApi(method, endpoint, requestData, setSpinning) {
   if (setSpinning != null) {
@@ -17,14 +16,14 @@ export async function callApi(method, endpoint, requestData, setSpinning) {
 
 async function doRestCall(method, endpoint, requestData) {
   try {
-    const requireProxy = process.env.NODE_ENV === "development" && window.location.hostname === "localhost";
-    const data = await fetch((requireProxy ? DEV_API_PROXY : API_URL) + endpoint, {
+    const isLocalTest = process.env.NODE_ENV === "development" && window.location.hostname === "localhost";
+    const data = await fetch(API_URL + endpoint, {
       method,
       body: JSON.stringify(requestData),
-      credentials: requireProxy && 'include'
+      credentials: isLocalTest && 'include'
     }).then(response => response.json());
 
-    return {data, error: extractError(data)};
+    return {data, errorCode: data.errorCode, error: extractError(data)};
   } catch (e) {
     console.error(e);
     return {error: "Internal error - " + e.message}
