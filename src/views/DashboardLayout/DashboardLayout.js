e1c4bb6892b8d3a580d6426d34d3587e6d3c6679
Florian <me@florian.ws>
2020-06-13 01:58:17 +0200

    Add user context

--- src/views/DashboardLayout/DashboardLayout.js
+++ src/views/DashboardLayout/DashboardLayout.js
@@ -1,30 +1,33 @@
-import React, {useState} from "react";
+import React, {useContext, useState} from "react";
 import classes from './DashboardLayout.module.less';
 import {Spin, Layout, Row, Col, Typography, Dropdown, Menu} from "antd";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Footer} from "../Footer/Footer";
-import {callApi} from "../../api/api";
+import {callApi, ApiDataLoader} from "../../shared/api";
 import {useHistory} from "react-router-dom";
+import {UserContext} from "../../shared/context";
 
 export const DashboardLayout = ({children}) => {
   return (
-    <Layout className={classes.outerLayout} >
-      <Layout.Header className={classes.header}>
-        <HeaderRow/>
-      </Layout.Header>
+    <ApiDataLoader endpoint="/auth/me" context={UserContext}>
+      <Layout className={classes.outerLayout}>
+        <Layout.Header className={classes.header}>
+          <HeaderRow/>
+        </Layout.Header>
 
-      <Layout>
-        <Layout.Sider width={200} className={classes.sider}>
-          Sider
-        </Layout.Sider>
+        <Layout>
+          <Layout.Sider width={200} className={classes.sider}>
+            Sider
+          </Layout.Sider>
 
-        <Layout.Content className={classes.content}>
-          {children}
-        </Layout.Content>
+          <Layout.Content className={classes.content}>
+            {children}
+          </Layout.Content>
+        </Layout>
+
+        <Footer/>
       </Layout>
-
-      <Footer/>
-    </Layout>
+    </ApiDataLoader>
   );
 };
 
@@ -46,15 +49,20 @@ const HeaderMenu = () => {
   );
 }
 
-const HeaderRow = () => (
-  <Row align="middle" className={classes.headerRow}>
-    <Col span={18} className={classes.headerTitleCol}>
-      <Typography.Link className={classes.headerTitle} href="https://g.reflex.rip/spigot" target="_blank">Reflex</Typography.Link>
-    </Col>
-    <Col span={6} className={classes.headerMenuCol}>
-      <Dropdown.Button overlay={<HeaderMenu/>} placement="bottomCenter" icon={<FontAwesomeIcon icon="user" />}>
-        Logged in as Name (12345)
-      </Dropdown.Button>
-    </Col>
-  </Row>
-);
+const HeaderRow = () => {
+  const user = useContext(UserContext);
+
+  return (
+    <Row align="middle" className={classes.headerRow}>
+      <Col span={18} className={classes.headerTitleCol}>
+        <Typography.Link className={classes.headerTitle} href="https://g.reflex.rip/spigot"
+                         target="_blank">Reflex</Typography.Link>
+      </Col>
+      <Col span={6} className={classes.headerMenuCol}>
+        <Dropdown.Button overlay={<HeaderMenu/>} placement="bottomCenter" icon={<FontAwesomeIcon icon="user"/>}>
+          Logged in as {user.username} ({user.userId})
+        </Dropdown.Button>
+      </Col>
+    </Row>
+  );
+}
