e2c8c5ebf10ebb06875f3079cd4afdd819df81ef
Florian <me@florian.ws>
2020-06-17 21:26:57 +0200

    add translation api

--- src/shared/context.js
+++ src/shared/context.js
@@ -5,3 +5,4 @@ export const WhitelistContext = React.createContext("whitelist");
 export const LoginHistoryContext = React.createContext("login-history");
 export const StartupHistoryContext = React.createContext("startup-history");
 export const IpInfoContext = React.createContext("ip-info");
+export const TranslationContext = React.createContext("dictionary");
---
+++ src/shared/i18n.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {useContext} from "react";
+import {TranslationContext} from "./context";
+
+export function useTranslation() {
+  const {translationInfo, updateTranslations} = useContext(TranslationContext);
+
+  if (translationInfo == null) {
+    throw new Error("Translation context unavailable");
+  }
+
+  return {
+    translationInfo,
+    updateTranslations,
+    t: (key) => translationInfo.getTranslation(key),
+  }
+}
+
+export const Trans = ({component, name}) => {
+  const [t] = useTranslation();
+  return component != null ? (<component>{t(name)}</component>) : (<>{t(name)}</>);
+}
+
+export class TranslationLoader extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      translationInfo: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch(this.props.src)
+      .then(result => result.json())
+      .then(languages => {
+        this.setState({
+          translationInfo: new TranslationInfo(languages)
+        });
+      });
+  }
+
+  render() {
+    return this.state.translationInfo ? (
+      <TranslationContext.Provider value={{
+        translationInfo: this.state.translationInfo,
+        updateTranslations: this.forceUpdate.bind(this)
+      }}>
+        {this.props.children}
+      </TranslationContext.Provider>
+    ) : (
+      <></>
+    )
+  }
+}
+
+class TranslationInfo {
+  constructor(languages) {
+    this.languages = languages;
+  }
+
+  getTranslation(key) {
+    const currentText = this.currentLanguage.keys[key];
+    if (currentText != null) return currentText;
+
+    const defaultText = this.defaultLanguage.keys[key];
+    if (defaultText != null) return defaultText;
+
+    return "[translation missing - you lazy fool]"
+  }
+
+  getLanguage(iso3) {
+    return iso3 == null ? null : this.languages.find(lang => lang["iso3"] === iso3);
+  }
+
+  get defaultLanguage() {
+    return this.languages.find(lang => lang.default === true);
+  }
+
+  get currentLanguage() {
+    return this.getLanguage(window.localStorage.getItem("language")) ?? this.defaultLanguage;
+  }
+
+  set currentLanguage(value) {
+    window.localStorage.setItem("language", value["iso3"]);
+  }
+}
